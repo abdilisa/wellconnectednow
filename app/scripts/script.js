ca5188fd96279745e085f10ce0ca152f3eab50ed
clairelyles <claire.lyles@gmail.com>
2015-07-08 14:35:10 -0700

    updated tabs for mentors, coaches and therapists

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -337,20 +337,6 @@
 		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
 
-		/* =======================================
-		 * Bootstrap TABS
-		 * =======================================
-		 */
-		// $('#mentors a').click(function (e) {
-		//   e.preventDefault()
-		//   $(this).tab('show')
-		// })
-		//
-		// $('#professionals a').click(function (e) {
-		//   e.preventDefault()
-		//   $(this).tab('show')
-		// })
-
 
 		$window.trigger( 'resize' );
 		$window.trigger( 'scroll' );
