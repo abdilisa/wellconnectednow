dada47544299fbb9e5f82daebeac12b801bf3dc5
abdilisa <abdilisa@gmail.com>
2015-09-30 15:47:41 -0700

    updated mail chimp & google API keys

--- routes/subscription.js
+++ routes/subscription.js
@@ -5,13 +5,14 @@ var express = require('express'),
   EmailSubscriptionUtility = require('../lib/emailSubscriptionUtility'),
   googleCreds;
 
-console.log('using mailchimp key:', '8c6223ca80f76410c3a10774dca4fa32-us10');
-var mc = new mcapi.Mailchimp('8c6223ca80f76410c3a10774dca4fa32-us10');
+console.log('using mailchimp key:', 'f9d80880acf0980522f97671dfb49ea1-us10');
+var mc = new mcapi.Mailchimp('f9d80880acf0980522f97671dfb49ea1-us10');
 
 googleCreds = {
-	spreadsheet_user: 'akaakaalex529@gmail.com',
-	spreadsheet_key: '1x9_Wen8GQV8-I3o4iEXjxs1y_B_l6VKXvyuX1IzdzGE'
+	client_email: '596794591195-na6uefl1h6h7ecibth4ubljvvojsqqvk@developer.gserviceaccount.com',
+	private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCgHdjvHza63dym\n1+fgItJvgk5u4tzPaSYs8O7kkN8iAkqiPKE87fZmd6rA8+sEd/4BNOCbzwwWRd8F\ndJLi1Vk/D6j7l5XPE9JmVgNF9xpyLP7qJyIWHZ/MOygHtGg+2ITxfJxSuEyl9UkK\nI3CbbPAEbZtyENDXddZi6muuqRXPDzXh23HkxeAdYISl+Z7OxI/L81F995zpyI31\n2SlSJeC4NsMwY3hhhDd58A1eZ+5YI9zqI230CpWDx70tsUpmexV4pKYqwh3p6Kbc\nnlUG+q6JPQ7FvMMn4+tId5en13xGC4QiKYetOASoQo1dZIx/isuU153m1iNbFRxo\nZUIwjBXJAgMBAAECggEBAI4pcg79m85WZi8My6fJwyWWgKUbcAoHxglo8fKmXjcH\n6dQaGN7MOVXJMiaXazdQgon3tOUh9A1ihB2H31LfMpo3AUZA1JZDdy+9drKML0Lo\niHzFuhIAw3zFmUkd+OCzxXXTucgIMqW3Ht6NkNGu63xDBR1FqOdkjLDOikVNgzuP\nUjZe6pBUzwLZiVOqgAyMmkuK55Wjbpy/diubFTd5IxLyq1x8qlb/9BYiVd7NpUDy\n+wTLOTbY4YEZD7Gmry5jokM0+dio4qBPviJ8iShw+yy55QKflWPHqOMdLMfuc+Tf\n9aNZolfVibYBCtjDR1LrcmC+udofBkwaYo617NCuZMECgYEAy0f39NuYJwN5MfJP\nKrmAkDRw6A/cupvNt/KgfD1QhEUDPAFN7jAvP4hvyMELquUcrYq9RCym5ifm3cqh\n7m6EMoXDvQg50WizV+FSNl56AquIYmM3vOC75K94xfJHUoRwM/uX70amWaCvyunL\nbMcrkFglXvhxH5n+FsGBC0hB0Y0CgYEAyaQks/aCfwLiAfTQm4HO2kz+YHzkSyLI\n7rT0/XVZTPsandYgHxeKYgQFbtzfyql1QvFZHTjn3O3p/CXg4QF4/JOkDT0y/HsS\n27Q97+5OFBMcgyZv3xt5IfNVyiMdaA5+WmVTrXoM+KyvNkZGCvHaV5q8XvTC4Zmh\nxYmAHk+zQC0CgYEAxlsCc7Y3IJJ+FyOW5ly+O3hAV+DqhFh1gzHIMZLu1zI0NsAb\nF/mCkR8D3MOqK+aGdjxz9GsRrHFxECiim/LRIi9uUS0OpezhWzYtL4o+yg4w9bhU\n8gPktAAY/CYuPDGo/sBYgA3TUo++GWTKr+kk9CNey+3nDGwKCJBm5+ho+R0CgYEA\npjM3VWZAFXRI2RfywdpypxiDKwCGIzAesjJRJICPBJaMT8BoZXg/xZ5O5BwGEcMv\nNeg8bYu0ATPQgtatzogqMvV2aLsixVykJJch3htsG7vM6oTJ/PlmFIe/FIdw+43G\ns7eFCx3ohye+m1u869SBBWgzD4lrAAxjfLBi+5jxxFkCgYBM7y4oneG4EbXJ1Rnv\ntIXCeBe+mbNqeO+7+vB6cGw/B2Nb5FB7ciplocOk00Tu3tvM6IibqFv2yNK+Qoft\nj5DcnMfcvSN3/MMCMOQZtEHRgilzhcCwKY4HtU1O/Cd+NAJsrWF24snCZj4yPrxj\ntM7VZZJ8EqBTuRztwnTumE0HXw\u003d\u003d\n-----END PRIVATE KEY-----\n'
 };
+
 
 var unverifiedSubscriptions = new EmailSubscriptionUtility(googleCreds);
 console.log('using google creds', googleCreds);
