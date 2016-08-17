123b7e37964b24ed0210075d82412926ca412e4f
abdilisa <lisa@wellconnectednow.com>
2016-08-16 22:04:16 -0700

    updating config.js for ltaa-blog file
    updating into ltaa-blog deploybot. debugging sql readonly bug

--- ltaa-blog/ghost-0.9.0/config.js
+++ ltaa-blog/ghost-0.9.0/config.js
@@ -31,8 +31,10 @@ config = {
         },
 
         server: {
-            host: '127.0.0.1',
-            port: '2368'
+            // Host to be passed to node's `net.Server#listen()`
+        host: 'localhost',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+        port: '8000'
         }
     },
 
@@ -40,7 +42,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'http://localhost:2368',
+        url: 'http://localhost:8000',
 
         // Example refferer policy
         // Visit https://www.w3.org/TR/referrer-policy/ for instructions
@@ -72,7 +74,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: '8000'
         },
         // #### Paths
         // Specify where your content directory lives
