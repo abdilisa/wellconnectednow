9ce702f31ff6e0659e4f1d1e0cccedb9296e4e58
abdilisa <abdilisa@wellconnectednow.com>
2016-11-18 12:31:31 -0800

    Ghost updates recording into GitHub
    Files without errors around ghost installation and setup. Themes
    included. troubleshooting errors around ghost blog commits

--- ltaa-blog/ghost-0.7.9/core/server/data/meta/published_date.js
+++
@@ -1,9 +0,0 @@
-function getPublishedDate(data) {
-    var context = data.context ? data.context[0] : null;
-    if (data[context] && data[context].published_at) {
-        return new Date(data[context].published_at).toISOString();
-    }
-    return null;
-}
-
-module.exports = getPublishedDate;
